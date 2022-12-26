c9e7738888d42b737747a6a1c949dc1ad749bcb0
potatooBot <kumarkeshav825@gmail.com>
2022-12-26 09:53:00 +0530

    Combination sum 2

--- src/Algorithm/Recursion/combination_sum2.java
+++ src/Algorithm/Recursion/combination_sum2.java
@@ -1,46 +1,31 @@
+//Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+//
+//        Each number in candidates may only be used once in the combination.
+//
+//       Note: The solution set must not contain duplicate combinations.
+//Time Complexity:O(2^n*k)
 package Algorithm.Recursion;
 import java.util.*;
 public class combination_sum2 {
-    public static void getCombination(int idx,int arr[],int target,int n, List<List<Integer>> ans,ArrayList <Integer> ds,int orig){
-        if(idx==n){
-if(target==0) {
-    Collections.sort(ds);
-  int sum=ds.stream().mapToInt(Integer ::intValue).sum();
-  if(sum==orig)
-    ans.add(new ArrayList<>(ds));
-    System.out.println(ans);
-
-}
-            return;
-        }
-        System.out.println(target);
-        if(arr[idx]<=target&&idx!=n){
-            ds.add(arr[idx]);
-            getCombination(idx+1,arr,target-arr[idx],n,ans,ds,orig);
+    public static void getCombination(int idx,int arr[],int target, List<List<Integer>> ans,ArrayList <Integer> ds){
+     if(target==0) {
+         ans.add(new ArrayList<>(ds));
+         return;
+     }
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]>target) break;
+            if (i>idx&&arr[i]==arr[i-1]) continue;
+            ds.add(arr[i]);
+            getCombination(i+1,arr,target-arr[i],ans,ds);
             ds.remove(ds.size()-1);
         }
-
-        getCombination(idx+1,arr,target,n,ans,ds,orig);
     }
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
-        ArrayList <Integer> ds=new ArrayList<>();
-        HashSet <List<Integer>> set=new HashSet<>();
-        int size=candidates.length;
-int orig=target;
-
-        getCombination(0,candidates,target,size,ans,ds,orig);
-        for (List ele:
-             ans) {
-            set.add(new ArrayList<>(ele));
-        }
-       ans.clear();
-
-        for(List ele:
-             set) {
-            ans.add(new ArrayList<>(ele));
-        }
-        return ans;
+     List <List<Integer>> ans=new ArrayList<>();
+     //First Sort it
+        Arrays.sort(candidates);
+     getCombination(0,candidates,8,ans,new ArrayList<>());
+     return ans;
     }
     public static void main(String[] args) {
 int arr[]={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
