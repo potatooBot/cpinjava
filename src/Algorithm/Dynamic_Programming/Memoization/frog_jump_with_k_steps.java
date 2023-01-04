074e418968d71ed51793ad7d0c52cbcaed3ac56e
potatooBot <kumarkeshav825@gmail.com>
2023-01-04 11:07:24 +0530

    Frog jump with k steps

--- src/Algorithm/Dynamic_Programming/Memoization/frog_jump_with_k_steps.java
+++ src/Algorithm/Dynamic_Programming/Memoization/frog_jump_with_k_steps.java
@@ -5,22 +5,24 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class frog_jump_with_k_steps {
-    public static int solve(int ind,int height[],int k){
+    public static int solve(int ind,int height[],int dp[],int k){
         if (ind==0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
 int minsteps=Integer.MAX_VALUE;
         for (int i = 1; i <=k ; i++) {
             if (ind >= i) {
-                int jump=solve(ind - i, height, k)+Math.abs(height[ind]-height[ind-i]);
+                         int jump=solve(ind - i, height,dp, k)+Math.abs(height[ind]-height[ind-i]);
                 minsteps=Math.min(minsteps,jump);
             }
         }
 return minsteps;
     }
     public static void main(String[] args) {
-        int height[]={10 ,10};
+        int height[]={10 ,30, 40, 50, 20};
         int n=height.length;
-//        int dp[]=new int[n];
-//        Arrays.fill(dp,-1);
-        System.out.println(solve(n-1,height,100));
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+int res=solve(n-1,height,dp,3);
+        System.out.println(res);
     }
 }
