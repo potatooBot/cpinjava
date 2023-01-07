85b1941bb168dca3552799bf95ea0c21befdb79b
potatooBot <kumarkeshav825@gmail.com>
2023-01-07 19:06:43 +0530

    Ninja Training Meimozation

--- src/Algorithm/Dynamic_Programming/Tabulation/ninja_training_2d.java
+++ src/Algorithm/Dynamic_Programming/Tabulation/ninja_training_2d.java
@@ -21,26 +21,12 @@ public class ninja_training_2d {
                     {30,60,90}};
 
 int n= points.length;
-   int dp[][]=new int[n][4];
-
-   dp[0][0]=Math.max(points[0][1],points[0][2]);
-   dp[0][1]=Math.max(points[0][0],points[0][2]);
-   dp[0][2]=Math.max(points[0][0],points[0][1]);
-   dp[0][3]=Math.max(points[0][0],Math.max(points[0][1],points[0][2]));
 
 
-            for (int day = 1; day <n ; day++) {
-                for (int last = 0; last<4 ; last++) {
-                    dp[day][last]=0;
-                    for (int task = 0; task <3 ; task++) {
-                        if(last!=task){
-                            dp[day][last]=Math.max(dp[day][last],points[day][task]+ dp[day-1][task]);
 
-                        }
-                    }
-                }
-            }
-            System.out.println(dp[n-1][3]);
+
+
+
         }
 
 }
