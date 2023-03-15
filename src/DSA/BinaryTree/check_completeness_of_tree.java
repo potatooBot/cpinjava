742bccd208f0fc88b242410beca8044025be5948
potatooBot <kumarkeshav825@gmail.com>
2023-03-15 22:21:24 +0530

    Check completeness of binary tree

---
+++ src/DSA/BinaryTree/check_completeness_of_tree.java
@@ -0,0 +1,39 @@
+package DSA.BinaryTree;
+import java.util.*;
+ class TreeNode3{
+      int val;
+      TreeNode3 left;
+      TreeNode3 right;
+      TreeNode3() {}
+      TreeNode3(int val) { this.val = val; }
+      TreeNode3(int val, TreeNode3 left, TreeNode3 right) {
+ this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class check_completeness_of_tree {
+
+    class Solution {
+        public boolean isCompleteTree(TreeNode root) {
+            if(root == null) return true;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+            boolean nullBw = false;
+            while(!q.isEmpty()){
+                int s = q.size();
+                for(int x = 0; x<s; x++){
+                    TreeNode curr = q.poll();
+                    if(curr == null) nullBw = true;
+                    else {
+                        if(nullBw) return false;
+                        q.offer(curr.left);
+                        q.offer(curr.right);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
