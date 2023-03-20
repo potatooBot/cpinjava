e357ec9c0ed1de2d3d1ce35aab1c515493b67cd5
potatooBot <kumarkeshav825@gmail.com>
2023-03-20 19:29:57 +0530

    Numberof ways to arrive a destination

--- src/Graph/Dijkstra_Algorithm/number_of_ways_to_arrive_at_destination.java
+++ src/Graph/Dijkstra_Algorithm/number_of_ways_to_arrive_at_destination.java
@@ -14,6 +14,10 @@ public class number_of_ways_to_arrive_at_destination {
           for (int i = 0; i <n ; i++) {
               ArrayList <pair_flights> temp=new ArrayList<>();
               adj.add(temp);
+          } 
+          for (int i = 0; i < roads.size() ; i++) {
+             adj.get(roads.get(i).get(0)).add(new pair_flights(roads.get(i).get(1),roads.get(i).get(2)));
+             adj.get(roads.get(i).get(1)).add(new pair_flights(roads.get(i).get(0),roads.get(i).get(2)));
           }
           int mod=(int)(1e9 +7);
           int distArr[]=new int[n];
@@ -33,7 +37,7 @@ public class number_of_ways_to_arrive_at_destination {
 
               int dist=it.distance;
               int node=it.node;
-
+pq.remove();
               for (pair_flights iter:
                    adj.get(node)) {
                   int adjNode=iter.node;
@@ -43,9 +47,9 @@ public class number_of_ways_to_arrive_at_destination {
                   if(dist+edjWt<distArr[adjNode]){
                       pq.add(new pair_flights(adjNode,edjWt+dist));
                       distArr[adjNode]=edjWt+dist;
-                      ways[adjNode]=node;
+                      ways[adjNode]=ways[node];
                   }
-                  if(dist+edjWt==distArr[adjNode]){
+                 else if(dist+edjWt==distArr[adjNode]){
                       ways[adjNode]=(ways[adjNode]+ways[node])%mod;
                   }
               }
@@ -56,7 +60,24 @@ public class number_of_ways_to_arrive_at_destination {
         // Your code here
     }
     public static void main(String[] args) {
-        int [][]edges= {{0,6,7},{0,1,2},{1,2,3},{1,3,3},{6,3,3},{3,5,1},{6,5,1},{2,5,1},{0,4,5},{4,6,2}};
-int n=7, m=10;
+        int n = 7;
+        List < List < Integer >> roads = new ArrayList < > () {
+            {
+                add(new ArrayList<Integer>(Arrays.asList(0, 6, 7)));
+                add(new ArrayList<Integer>(Arrays.asList(0, 1, 2)));
+                add(new ArrayList<Integer>(Arrays.asList(1, 2, 3)));
+                add(new ArrayList<Integer>(Arrays.asList(1, 3, 3)));
+                add(new ArrayList<Integer>(Arrays.asList(6, 3, 3)));
+                add(new ArrayList<Integer>(Arrays.asList(3, 5, 1)));
+                add(new ArrayList<Integer>(Arrays.asList(6, 5, 1)));
+                add(new ArrayList<Integer>(Arrays.asList(2, 5, 1)));
+                add(new ArrayList<Integer>(Arrays.asList(0, 4, 5)));
+                add(new ArrayList<Integer>(Arrays.asList(4, 6, 2)));
+
+            }
+
+        };
+        int ans=countPaths(n,roads);
+        System.out.println(ans);
     }
 }
