09681b6dc70c885e808cedfea76d87f8197e56f2
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-04-10 13:47:25 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+     ArrayList<Integer> list=new ArrayList<>();
+        Node cur=head;
+        
+        
+        while(cur!=null){
+            list.add(cur.data);
+            cur=cur.next;
+        }
+        
+        Collections.sort(list);
+    //    System.out.println(list);
+        Node dummy=new Node(0);
+        Node cur2=dummy;
+        for(int i=0;i<list.size();i++){
+            
+            Node temp=new Node(list.get(i));
+        cur2.next=temp;
+            cur2=cur2.next;
+            
+        }
+        
+        return dummy.next;
+    }
+}
+
+
