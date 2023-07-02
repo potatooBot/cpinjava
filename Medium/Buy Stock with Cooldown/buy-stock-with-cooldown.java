368b34b9a9d1f5f80583d1452ad2d74afe6026a3
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-07-02 12:03:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Stock with Cooldown/buy-stock-with-cooldown.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int N=sc.nextInt();
+            long []prices=new long[N];
+            for(int i=0;i<N;i++){
+                prices[i]=sc.nextLong();
+            }
+            Solution obj=new Solution();
+            long res=obj.maximumProfit(prices,N);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public long maximumProfit(long prices[], int N){
+        //Code here
+         int n=prices.length;
+
+
+ long dp[][]=new long [n+2][2];
+   /*/     for (int i = 0; i < n; i++) {
+         for (int j = 0; j < 2; j++) {
+            Arrays.fill(dp[i][j], -1);
+         }
+        }
+*/
+
+for(int idx=n-1;idx>=0;idx--){
+
+   for(int buy=0;buy<=1;buy++){
+  
+
+if(buy==0){
+
+ dp[idx][buy]=Math.max(-prices[idx]+dp[idx+1][1],0+dp[idx+1][0]);
+}
+if(buy==1){
+   dp[idx][buy]=Math.max(prices[idx]+dp[idx+2][0],0+dp[idx+1][1]);
+}
+
+}
+}
+
+return dp[0][0];
+    }
+}
