7e85d3a56b7b6d8a4482ac1f5b33ba4bdf3d5f61
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-07-22 01:24:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the element that appears once/find-the-element-that-appears-once.java
+++ Medium/Find the element that appears once/find-the-element-that-appears-once.java
@@ -31,25 +31,40 @@ class GfG
 
 class Sol
 {
-    public static int search(int nums[], int N)
+    public static int search(int arr[], int n)
     {
-        // your code here
-         int low=0;
-       int high= nums.length-2;
+        
+        if(n==1) {
+            return arr[0];
+        }
+        if(arr[0]!=arr[1]){
+            return arr[0];
+        }
+        if(arr[n-1]!=arr[n-2]){
+            return arr[n-1];
+        }
 
-       while (low<=high) {
-           int mid = (low + high) / 2;
-           if (mid % 2 == 0) {
-               if (nums[mid] != nums[mid + 1]) {
-                   high = mid - 1;
-               } else low = mid + 1;
-           }
-           else {
-               if (nums[mid] == nums[mid + 1]) {
-                   high = mid - 1;
-               } else low = mid + 1;
-           }
-       }
-        return nums[low];
+        int low=1;
+        int high=n-2;
+
+        while (low<=high){
+            int mid=(low+high)/2;
+
+
+            if(arr[mid]!=arr[mid-1]&&arr[mid]!=arr[mid+1]){
+                return arr[mid];
+            }
+
+
+            if((mid%2==1&&arr[mid]==arr[mid-1]) || mid%2==0&&arr[mid]==arr[mid+1])   {
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+
+        }
+        return -1;
+        // your code here
     }
 }
