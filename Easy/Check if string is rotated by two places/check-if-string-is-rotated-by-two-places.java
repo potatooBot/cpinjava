c7819b2830ead586dabadf8c514cbda25e12815b
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-08-09 19:45:09 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check if string is rotated by two places/check-if-string-is-rotated-by-two-places.java
+++ Easy/Check if string is rotated by two places/check-if-string-is-rotated-by-two-places.java
@@ -29,40 +29,64 @@ class GFG {
 
 class Solution
 {
+        public static void reverse(char arr[],int start,int end){
+
+
+        while(start<=end){
+            char temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void checkLeft(char arr[]){
+        reverse(arr,0,1);
+        reverse(arr,2,arr.length-1);
+        reverse(arr,0,arr.length-1);
+
+    }
+    public static void  checkRight(char arr[]){
+        reverse(arr,0,arr.length-1);
+        reverse(arr,0,1);
+        reverse(arr,2,arr.length-1);
+    }
     //Function to check if a string can be obtained by rotating
     //another string by exactly 2 places.
-    public static boolean isRotated(String str1, String str2)
+    public static boolean isRotated(String str, String str2)
     {
-        // Your code here
-        if(str1.length()<2||str2.length()<2) return false;
-        char charArr[]=str1.toCharArray();
-        for (int i = 0; i <2 ; i++) {
-            char temp=charArr[0];
+        if(str.length()!=str2.length()){
+   return false;
+}
+if(str.length()==1&&str2.length()==1){
+    if(str.equals(str2)) return true;
+   return false;
+}
+        int n=str.length();
+char arr[]=new char[n];
+arr=str.toCharArray();
 
-            for (int j = 0; j <str1.length()-1 ; j++) {
-                charArr[j]=charArr[j+1];
-
-            }
-            charArr[str1.length()-1]=temp;
-    
-        }
-                String str=new String(charArr);
-if (str.equals(str2)) return true;
-        charArr=str1.toCharArray();
-        String strAns=new String(charArr);
-        //System.out.println(strAns);
-        for (int i = 0; i <2 ; i++) {
-            char temp=charArr[str1.length()-1];
-
-            for (int j = str1.length()-1; j >0 ; j--) {
-              charArr[j]=charArr[j-1];
-            }
-            charArr[0]=temp;
-        
-    }
-        strAns=new String(charArr);
-         
-   if (strAns.equals(str2)) return true;
+        char arr2[]=new char[n];
+        arr2=str.toCharArray();
+checkLeft(arr);
+checkRight(arr2);
+String res1=new String(arr);
+String res2=new String(arr2);
+boolean ans1=false;
+boolean ans2=false;
+if(res1.equals(str2)){
+    ans1=true;
+}
+if(res2.equals(str2)){
+    ans2=true;
+}
+if(ans1||ans2){
+    return true;
+}
+else {
     return false;
 }
+
+    }
+    
 }
