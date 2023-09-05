88e3228c5b3cbedd1379e74ad9c278e2e7effd2d
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-09-06 01:13:31 +0530

    Time: 107 ms (46.15%), Space: 55 MB (90.73%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,40 +1,51 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-              Stack<Integer> stk=new Stack<>();
-        int leftBoundary[]=new int[heights.length];
-        for (int i = 0; i < heights.length ; i++) {
-            while (stk.isEmpty()==false&&heights[stk.peek()]>=heights[i]){
-                stk.pop();
-            }
-            if(stk.isEmpty()==true) {
-                leftBoundary[i]=0;
-            }
-            else {
-                leftBoundary[i]=stk.peek()+1;
-            }
-            stk.push(i);
+    public int largestRectangleArea(int[] arr) {
+               Stack <Integer> stk=new Stack<>();
+        int n=arr.length;
+
+        int left[]=new int[n+1];
+        int right[]=new int[n+1];
+
+
+        for(int i=0;i<n;i++){
+
+while(!stk.isEmpty()&&arr[stk.peek()]>=arr[i]){
+    stk.pop();
+}
+
+if(stk.isEmpty()){
+    left[i]=0;
+}
+else{
+    left[i]=stk.peek()+1;;
+}
+stk.push(i);
+
         }
+
 
         stk.clear();
-        int rightBoundary[]=new int[heights.length];
-        for (int i = heights.length-1; i >=0 ; i--) {
-            while (stk.isEmpty()==false&&heights[stk.peek()]>=heights[i]){
-                stk.pop();
-            }
-            if(stk.isEmpty()==true) {
-                rightBoundary[i]= heights.length-1;
-            }
-            else {
-                rightBoundary[i]=stk.peek()-1;
-            }
-            stk.push(i);
+
+
+        for(int i=n-1;i>=0;i--){
+while(!stk.isEmpty()&&arr[stk.peek()]>=arr[i]){
+    stk.pop();
+}
+if(stk.isEmpty()){
+    right[i]=n-1;
+}
+else{
+    right[i]=stk.peek()-1;
+}
+stk.push(i);
         }
 
-      int maxi=Integer.MIN_VALUE;
-        for (int i = 0; i < heights.length ; i++) {
-            maxi=Math.max(maxi,(rightBoundary[i]-leftBoundary[i]+1)*heights[i]);
-        }
+int maxA=0;
+for(int i=0;i<n;i++){
+    maxA=Math.max(maxA,(right[i]-left[i]+1)*arr[i]);
+}
 
-        return maxi;
+
+return maxA;
     }
 }
