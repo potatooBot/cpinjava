92bde229ab602688bb59f6ef401c387b489df604
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-09-08 13:23:57 +0530

    Time: 6 ms (5.8%), Space: 41.5 MB (7.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,20 +1,23 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> ans=new ArrayList<>();
-     ans.add(new ArrayList<>());
-     ans.get(0).add(1);
-     for (int rows = 1; rows <numRows ; rows++) {
-         List<Integer> rowArr=new ArrayList<>();
-         List<Integer> prevRow=new ArrayList<>(ans.get(rows-1));
-     rowArr.add(1);
+    public List<List<Integer>> generate(int rows) {
+     List<Integer> ds=new ArrayList<>();
 
-         for (int j = 1; j <rows ; j++) {
-             rowArr.add(prevRow.get(j-1)+prevRow.get(j));
-         }
-         rowArr.add(1);
-                ans.add(new ArrayList<>(rowArr));
+     List<List<Integer>> ans=new ArrayList<>(); 
 
-     }
-     return ans;
+ds.add(1);
+ans.add(new ArrayList<>(ds));
+for(int i=1;i<rows;i++){
+List<Integer> prev=ans.get(i-1);
+System.out.println(ans.get(i-1));
+List<Integer> list=new ArrayList<>();
+list.add(1);
+for(int j=0;j<prev.size()-1;j++){
+list.add(prev.get(j)+prev.get(j+1));
+}
+list.add(1);
+ans.add(new ArrayList<>(list));
+}
+
+return ans;
     }
 }
