d324484dcd8c2b246f2356ff56bab9cdb6f6e854
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-09-14 01:12:19 +0530

    Time: 28 ms (85.61%), Space: 61.6 MB (25.3%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n=stones.length;
+        int dp[][]=new int[n][n];
+
+        for(int rows[]:dp)
+        Arrays.fill(rows,-1);
+    
+    return getJump(stones,0,0,dp)==1;
+    }
+
+
+    public static int getJump(int stones[],int idx,int jump,int dp[][]){
+if(dp[idx][jump]>=0) {
+    return dp[idx][jump];
+}
+
+
+
+
+
+int idx2=Arrays.binarySearch(stones,idx+1,stones.length,stones[idx]+jump);
+if(idx2>=0&&getJump(stones,idx2,jump,dp)==1){
+    return dp[idx][jump]=1;
+}
+
+int idx1=Arrays.binarySearch(stones,idx+1,stones.length,stones[idx]+jump-1);
+if(idx1>=0&&getJump(stones,idx1,jump-1,dp)==1){
+    return dp[idx][jump-1]=1;
+}
+
+
+
+
+int idx3=Arrays.binarySearch(stones,idx+1,stones.length,stones[idx]+jump+1);
+if(idx3>=0&&getJump(stones,idx3,jump+1,dp)==1){
+    return dp[idx][jump+1]=1;
+}
+
+
+
+if(idx==stones.length-1){
+    dp[idx][jump]=1;
+}
+else{
+    dp[idx][jump]=0;
+}
+
+
+return dp[idx][jump];
+    }
+}
