5a487f65fb20fc39e02b549d09c225b2d65ae069
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-09-24 17:10:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Consecutive Ones/maximum-consecutive-ones.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.longestOnes(n, nums, k);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int longestOnes(int n, int[] nums, int k) {
+        int zeroCnt=0;
+      //  int n=nums.length;
+int ans=0;
+int start=0;
+    for(int end=0;end<n;end++){
+if(nums[end]==0){
+    zeroCnt++;
+}
+while(zeroCnt>k){
+    if(nums[start]==0){
+        zeroCnt--;
+    }
+    start++;
+}
+
+ans=Math.max(ans,end-start+1);
+    }
+    return ans;
+    }
+}
+        
