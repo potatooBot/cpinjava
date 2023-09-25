cb04db868d8c6cae284802c63a60666cb7e0fec6
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-09-26 01:29:02 +0530

    Time: 29 ms (12.63%), Space: 56.3 MB (5.04%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -3,7 +3,7 @@ class Solution {
  int n=matrix.length;
  int m=matrix[0].length;
  int mat[][]=new int[n][m];
- for (int i = 0; i < n; i++) {
+/* for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
   if(matrix[i][j]=='0'){
       mat[i][j]=0;
@@ -20,18 +20,20 @@ class Solution {
          int dp[][]=new int[n][m];
         
         for(int j=0;j<m;j++){
-            dp[0][j]=mat[0][j];
+  if(matrix[0][j]=='1')          
+            dp[0][j]=1;
         }
         
         
         for(int i=0;i<n;i++){
-            dp[i][0]=mat[i][0];
+           if(matrix[i][0]=='1')
+            dp[i][0]=1;
         }
         
         
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-     if(mat[i][j]==0){
+     if(matrix[i][j]=='0'){
          dp[i][j]=0;
      }       
      else{
