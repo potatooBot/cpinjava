32372b80ac6a5e5c6be1a61b22d1a2a535e4f689
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2023-10-03 19:57:38 +0530

    Time: 1 ms (86.48%), Space: 39.8 MB (60.02%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+    int cnt=0;
+for(int i=0;i<nums.length-1;i++){
+    for(int j=i+1;j<nums.length;j++){
+if(nums[i]==nums[j]) cnt++;
+    }
+}
+return cnt;
+    }
+}
