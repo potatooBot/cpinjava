62e3f66ee1961e91982559360c9cff6450a7bdee
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2024-01-28 21:07:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(input[0]);
+            int x = Integer.parseInt(input[1]);
+            
+            int mat1[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat1[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+            int mat2[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat2[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+        
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countPairs(mat1,mat2,n,x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        HashSet<Integer> set1=new HashSet<>();
+           HashSet<Integer> set2=new HashSet<>();
+        for(int i=0;i<mat1.length;i++){
+             
+        for(int j=0;j<mat1[0].length;j++){
+            set1.add(mat1[i][j]);
+            set2.add(mat2[i][j]);
+        } 
+        }
+        
+        int ans=0;
+        
+             for(int ele:set1){
+     
+         if(set2.contains(x-ele))
+         ans++;
+        
+        }
+        return ans;
+    }
+}
