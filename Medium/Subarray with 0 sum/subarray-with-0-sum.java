195772e28bac5433205b8ee2acdb1e87324f497d
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2024-03-14 18:15:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarray with 0 sum/subarray-with-0-sum.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+			//code
+			
+		//taking input using class Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//taking total number of testcases
+		int t = scan.nextInt();
+		while(t>0)
+		{
+		    //taking total count of elements
+		    int n = scan.nextInt() ;
+		    
+		    //Declaring and Initializing an array of size n
+		    int arr[] = new int[n];
+		    
+		    //adding elements to the array
+		    for(int i = 0; i<n;i++)
+		    {
+		        arr[i] = scan.nextInt();
+		    }
+		    
+		    t--;
+		    
+		     //calling the method findSum
+		     //and print "YES" or "NO" based on the result
+		     System.out.println(new Solution().findsum(arr,n)==true?"Yes":"No");
+		}
+	}
+	
+	
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int nums[],int n)
+    {
+        HashMap<Integer,Integer> map=new HashMap<>();
+map.put(0,1);
+int sum=0;
+int cnt=0;
+for(int ele:nums){
+    if(ele==0) return true;
+    sum=sum+ele;
+
+if(map.containsKey(sum)){
+   return true;
+}
+map.put(sum,map.getOrDefault(sum,0)+1);
+
+}
+return false;
+    }
+}
