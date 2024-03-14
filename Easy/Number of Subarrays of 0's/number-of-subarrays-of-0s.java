512577a06d03114d3ab3dfc7ba49b151b61ff0e2
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2024-03-14 18:20:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of Subarrays of 0's/number-of-subarrays-of-0s.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int [] arr = new int[N];
+			String [] str = br.readLine().trim().split(" ");
+			for(int i= 0; i < N; i++)
+				arr[i] = Integer.parseInt(str[i]);
+			Solution obj = new Solution();
+			System.out.println(obj.no_of_subarrays(N, arr));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	long no_of_subarrays(int N, int [] nums) {
+	  long cnt=0;
+long zeroCnt=0;
+        for(int ele:nums){
+if(ele==0){
+zeroCnt++;
+cnt=cnt+zeroCnt;
+}
+else{
+    zeroCnt=0;
+}
+        }
+    return cnt;
+	}
+}
