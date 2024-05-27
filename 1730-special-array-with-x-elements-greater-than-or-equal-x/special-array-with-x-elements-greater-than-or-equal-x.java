91185c354aa78dfbaf5088cb37c45bfab7ba7f4b
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2024-05-28 01:01:52 +0530

    Time: 1 ms (78.32%) | Memory: 41 MB (83.20%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        for(int i=0;i<=nums.length;i++){
+int cnt=0;
+
+for(int j=0;j<nums.length;j++){
+    if(nums[j]>=i) cnt++;
+}
+if(cnt==i) return i;
+
+        }
+
+        return -1;
+    }
+}
