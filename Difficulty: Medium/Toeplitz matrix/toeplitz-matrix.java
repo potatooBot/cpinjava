b9bb4946cabf70d861f015d3ece3f7cf6a213afb
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2024-06-27 20:53:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Toeplitz matrix/toeplitz-matrix.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Check_IsToepliz {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int arr[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) arr[i][j] = sc.nextInt();
+            }
+
+            Solution g = new Solution();
+            boolean b = g.isToeplitz(arr);
+
+            if (b == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    /*You are required to complete this method*/
+    boolean isToeplitz(int mat[][]) {
+        // Your code here
+      
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(i-1>=0&&j-1>=0){
+                    if(mat[i][j]!=mat[i-1][j-1])
+                    return false;
+                }
+            }
+            
+        }
+       
+        return true;
+    }
+}
