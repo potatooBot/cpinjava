0b31948ed84e9f4210a137f299196ada810407d6
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-07-19 21:27:29 +0530

    Time: 346 ms (18.92%) | Memory: 64.8 MB (5.56%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public long minimumCost(int m, int n, int[] hor, int[] ver) {
+                ArrayList<Integer> hori=new ArrayList<>();
+        ArrayList<Integer> vert=new ArrayList<>();
+for(int ele:hor) hori.add(ele);
+for(int ele:ver) vert.add(ele);
+Collections.sort(hori,Collections.reverseOrder());
+Collections.sort(vert,Collections.reverseOrder());
+
+    int i=0;
+    int j=0;
+int horCuts=1;
+int vertCuts=1;
+long ans=0;
+while(i<hori.size()&&j<vert.size())
+    {
+        if(hori.get(i)>=vert.get(j)){
+ans=ans+ vertCuts*hori.get(i);
+        i++;
+        horCuts++;
+        }
+        else{
+ans=ans+horCuts*vert.get(j);
+j++;
+vertCuts++;
+        }
+    }
+
+
+    while(i<hori.size()){
+        ans=ans+ vertCuts*hori.get(i);
+        i++;
+        horCuts++;
+    }
+
+    while(j<vert.size()){
+        ans=ans+horCuts*vert.get(j);
+j++;
+vertCuts++;
+    }
+    return ans;
+    }
+}
