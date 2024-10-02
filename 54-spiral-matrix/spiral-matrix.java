0685c381ccf25c1ea9cae11dc27690f4a7f06130
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-10-02 19:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (64.71%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] mat) {
+     //R -D-L-U
+     //Visited Array
+
+int m=mat.length;
+int n=mat[0].length;
+
+List<Integer> ans=new ArrayList<>();
+
+
+int dr[]={0,1,0,-1};
+int dc[]={1,0,-1,0};
+int row=0;
+int col=0;
+int di=0;
+
+boolean vis[][]=new boolean[m][n];
+
+for(int i=0;i<(n*m);i++){
+ans.add(mat[row][col]);
+vis[row][col]=true;
+int neiRow= row  + dr[di];  // 0 + 0=0    // 0 + 0 = 0  // 0 +0 =0
+int neiCol= col +dc[di];    // 0 + 1=1   // 1 + 1 = 2   // 2+1=3
+
+//
+
+if(neiRow>=0&& neiRow<m && neiCol>=0 && neiCol<n && vis[neiRow][neiCol]==false)
+{
+row=neiRow;
+col=neiCol;
+}
+else {
+ di=(di+1)%4;
+  row= row+dr[di];
+  col= col+ dc[di];
+}
+
+
+}
+return ans;
+     
+
+
+    }
+}
