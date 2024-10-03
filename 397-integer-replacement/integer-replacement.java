2a22afc0e3c52813e5f1f0766b9357aa351f1f50
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-10-03 19:22:56 +0530

    Time: 16 ms (5.61%) | Memory: 45.1 MB (5.11%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -17,13 +17,14 @@ class Solution {
             int cnt=q.peek().cnt;
             q.remove();
             if(val==1) return cnt;
-            if(val%2==0){
-                q.add(new pair(val/2,cnt+1));
-            }
+         if(val%2==0){
+            q.add(new pair(val/2,cnt+1));
+         }
             else{
-                 q.add(new pair(val+1,cnt+1));
-                  q.add(new pair(val-1,cnt+1));
+                q.add(new pair(val+1,cnt+1));
+                q.add(new pair(val-1,cnt+1));
             }
+         
         }
 
         return -1;
