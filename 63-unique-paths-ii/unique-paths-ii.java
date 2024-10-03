21113bc7793d872bbb7a10766b631a5df165c2fa
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-10-04 00:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (52.76%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+public static int memo(int mat[][],int row,int col,int dp[][]){
+   
+   if(row<0||col<0) return 0;
+    if(row>=0&&col>=0&&mat[row][col]==1){
+    return 0;
+}
+if(row==0&&col==0) return 1;
+if(dp[row][col]!=-1) return dp[row][col];
+
+ int up=memo(mat,row-1,col,dp);
+ int left=memo(mat,row,col-1,dp);
+
+
+ return dp[row][col]=up+left;
+}
+
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+
+        int dp[][]=new int[n][m];
+
+        for(int rows[]:dp){
+            Arrays.fill(rows,-1);
+        }
+       return memo(mat,n-1,m-1,dp);
+    }
+}
