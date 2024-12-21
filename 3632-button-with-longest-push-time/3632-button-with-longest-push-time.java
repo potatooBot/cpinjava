79e19e53acfd9e37f930032be55caccadd2b8718
Keshav <81036483+potatooBot@users.noreply.github.com>
2024-12-21 16:49:28 +0530

    Time: 10 ms (6.33%), Space: 45 MB (22%) - LeetHub

---
+++ 3632-button-with-longest-push-time/3632-button-with-longest-push-time.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int buttonWithLongestTime(int[][] events) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+    int time=0;
+        for(int arr[]:events){
+int cal=arr[1]-time;
+time=arr[1];
+if(map.containsKey(cal)==true)
+{
+    if(map.get(cal)>arr[0])
+    map.put(cal,arr[0]);
+}
+else{
+    map.put(cal,arr[0]);
+}
+        }
+
+System.out.println(map);
+        int ans=0;
+        int tm=0;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+       tm=Math.max(tm,entry.getKey());
+ 
+ 
+        }
+ return map.get(tm);
+    }
+}
